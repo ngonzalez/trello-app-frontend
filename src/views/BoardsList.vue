7a70086ab1a5f6d5e5a36bba8a3b3cbb9dfc7156
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2022-05-28 01:10:38 +0200

    Add BoardsList

--- src/views/BoardsList.vue
+++ src/views/BoardsList.vue
@@ -1,12 +1,51 @@
 <template>
-  <DisplayBoardsList></DisplayBoardsList>
+  <v-card class="container">
+    <v-container fluid>
+      <v-row>
+        <v-col cols="12">
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-breadcrumbs :items="breadcrumbs">
+            <template v-slot:divider>
+              <v-icon>mdi-chevron-right</v-icon>
+            </template>
+          </v-breadcrumbs>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-card>
 </template>
 
 <script>
-  import DisplayBoardsList from '../components/DisplayBoardsList.vue';
-
   export default {
     name: 'BoardsList',
-    components: { DisplayBoardsList },
+    components: { },
+    data() {
+      return {
+        breadcrumbs: [],
+        boards: [],
+      }
+    },
+    created() {
+      this.breadcrumbs = [];
+      this.breadcrumbs.push({
+        disabled: false,
+        text: this.$t('boards.title'),
+        to: {
+          name: 'boards'
+        },
+      });
+    },
+    watch:{
+      '$route.name': {
+        handler: function(route_name) {
+          console.log(route_name);
+        }
+      }
+    },
+    methods: {
+    }
   };
 </script>
